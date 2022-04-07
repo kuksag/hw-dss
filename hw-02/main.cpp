c2273ae82a23292bbded6ada7a43a1548b16f123
DymashevskijSP <sp.dymashevskij@gmail.com>
2022-04-07 03:47:56 +0300

    changed CMakeLists.txt to finally compile

--- hw-02/main.cpp
+++ hw-02/main.cpp
@@ -1,3 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <zstd.h>
+#include <lz4.h>
+#include "dependencies/zstd/examples/common.h"
+#include <experimental/filesystem>
+#include <fstream>
+
+//usage of ZSTD -- http://facebook.github.io/zstd/zstd_manual.html
+//ZSTD_compress(void* dst, size_t dstCapacity, const void *src, size_t srcSize, int compressLevel) to compress
 int main(){
     return 0;
-}
+};
